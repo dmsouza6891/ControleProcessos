fdde94706fce8fd8407c7672248fc780111052cc
Demethrios Miranda de Souza <DemethriosSouza@Prefeitura.local>
2021-08-07 15:48:12 -0300

    JanelaRequerimentos

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,17 +9,22 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	public static Scene mainScene;
 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			 FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml")); //instancia o carregador da view passando o caminho
 			 ScrollPane scrollPane = loader.load();
-			 scrollPane.setFitToHeight(true);
+			 
+			 scrollPane.setFitToHeight(true); //ajusta a largura do menu na janela
 			 scrollPane.setFitToWidth(true);
-			 Scene mainScene = new Scene(scrollPane); 
+			 
+			 mainScene = new Scene(scrollPane); 
 			 primaryStage.setScene(mainScene); //define a cena utilizada para tela principal
 			 primaryStage.setTitle("Gestão de Tarefas"); //define texto para a tela principal
+			 primaryStage.setMaximized(true); //define para ocupar a tela inteira
 			 primaryStage.show(); //mostra a tela principal
 		} catch (IOException e) {
 			 e.printStackTrace();
@@ -29,4 +34,9 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class MainViewController implements Initializable {
  
@@ -20,7 +29,9 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemRequisicoesAction() {
-		System.out.println("Requisicoes");
+		
+		createDialogForm("/gui/RequisicaoForm.fxml", Main.getMainScene() ,"Cadastro de Requisições");
+	
 	}
 	
 	@FXML
@@ -30,6 +41,24 @@ public class MainViewController implements Initializable {
 	
 	public void initialize(URL uri, ResourceBundle rb) {
 		
+	}
+	
+	private void createDialogForm(String absoluteName, Scene parentStage, String titleDialog) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setTitle(titleDialog);
+			dialogStage.initOwner(parentStage.getWindow()); //define a Janela que será a raiz da Janela Modal
+			dialogStage.setResizable(false); //não permite que a janela seja maximizada ou minimizada
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		}
+		catch(IOException e){
+			Alerts.showAlert("IOException", "Erro ao carregar janela", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
